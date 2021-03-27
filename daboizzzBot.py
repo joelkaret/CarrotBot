336caa7bd3103e0479299c20d55953584cb6678b
joelkaret <81484847+joelkaret@users.noreply.github.com>
2021-03-27 18:17:04 +0000

    Update daboizzzBot.py

--- daboizzzBot.py
+++ daboizzzBot.py
@@ -9,12 +9,13 @@ client = commands.Bot(command_prefix='&&')
 async def on_ready():
     print('Bot is ready')
 
+
 @client.command()
 async def ping(ctx, *, message="hi"):
     daboiz = 799036164057071636
     role = get(ctx.guild.roles, id=daboiz)
     if role in ctx.author.roles:
-        await ctx.send(role.mention + ctx.author.mention +" says " + message)
+        await ctx.send(role.mention + ctx.author.mention + " says " + message)
 
 
 client.run("ODI1 tMzU5NDM0NjAyNTEyMzg4.YF8x3w.8MzJhl2xYl6lnIOlG0WGNxosaNQ")
