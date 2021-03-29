5e4887ba2e11ff69525357f8521b4aaf08348095
Joel Karet <joelkaret@gmail.com>
2021-03-29 22:13:03 +0100

    Added slash commands

--- daboizzzBot.py
+++ daboizzzBot.py
@@ -5,11 +5,12 @@ from discord.utils import get
 from discord_slash import SlashCommand, SlashContext
 
 client = commands.Bot(command_prefix='&&')
-slash = SlashCommand(bot)
+slash = SlashCommand(client, sync_commands=True)
+guild_ids = [713646548436910116]
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.online, activity=discord.Game('&&ping To @da boiz'))    
+    await client.change_presence(status=discord.Status.online, activity=discord.Game('&&ping To @da boiz'))
     print('Bot is ready')
 
 
@@ -24,8 +25,9 @@ async def ping(ctx, *message):
     if role in ctx.author.roles:
         await ctx.send(role.mention + ctx.author.mention + " says " + " ".join(message))
 
+
 @slash.slash(name="pingDaBoiz")
-async def ping(ctx, *message):
+async def ping2(ctx, *message, guild_ids=guild_ids):
     daboiz = 799036164057071636
     role = get(ctx.guild.roles, id=daboiz)
     for i in range(0, len(message)):
@@ -34,9 +36,16 @@ async def ping(ctx, *message):
                 role = "this is not a role"
     if role in ctx.author.roles:
         await ctx.send(role.mention + ctx.author.mention + " says " + " ".join(message))
-    
-@slash.slash()
+
+
+@client.command()
 async def testWhoop(ctx, *message):
     await ctx.send(ctx.author.mention + " WHOOP!")
+
+
+@slash.slash(name="testWhoop", guild_ids=guild_ids)
+async def testWhoop2(ctx, *message):
+    await ctx.send(ctx.author.mention + " WHOOP!")
+
 
 client.run(os.getenv("PRIVATE_KEY"))
