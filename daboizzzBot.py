f525ce1d9ebe1433355712628e3271fcd5861762
Joel Karet <joelkaret@gmail.com>
2021-03-29 23:17:04 +0100

    removing built in help command, added own command

--- daboizzzBot.py
+++ daboizzzBot.py
@@ -7,14 +7,23 @@ from discord_slash import SlashCommand, SlashContext
 client = commands.Bot(command_prefix='&&')
 slash = SlashCommand(client, sync_commands=True)
 guild_ids = [713646548436910116]
+client.remove_command("help")
 
 @client.event
 async def on_ready():
     await client.change_presence(status=discord.Status.online, activity=discord.Game('&&ping To @da boiz'))
     print('Bot is ready')
 
-
 @client.command()
+async def help(ctx):
+    await ctx.send("This is a temporary help command")
+
+@slash.slash(name="help", guild_ids=guild_ids, description="Displays the help menu")
+async def helpSlash(ctx):
+    await ctx.send("This is a temporary help command")
+
+
+@client.command(name="ping", description="pings da boiz")
 async def ping(ctx, *message):
     daboiz = 799036164057071636
     role = get(ctx.guild.roles, id=daboiz)
@@ -25,9 +34,8 @@ async def ping(ctx, *message):
     if role in ctx.author.roles:
         await ctx.send(role.mention + ctx.author.mention + " says " + " ".join(message))
 
-
 @slash.slash(name="pingDaBoiz", guild_ids=guild_ids, description="This will ping da boiz")
-async def ping2(ctx, *message):
+async def pingSlash(ctx, *message):
     daboiz = 799036164057071636
     role = get(ctx.guild.roles, id=daboiz)
     for i in range(0, len(message)):
@@ -38,13 +46,13 @@ async def ping2(ctx, *message):
         await ctx.send(role.mention + ctx.author.mention + " says " + " ".join(message))
 
 
-@client.command()
-async def testWhoop(ctx, *message, description="This will WHOOP your ass!"):
+@client.command(name="Whoop", description="This will WHOOP your ass!")
+async def Whoop(ctx, *message):
     await ctx.send(ctx.author.mention + " WHOOP!")
 
 
-@slash.slash(name="testWhoop", guild_ids=guild_ids)
-async def testWhoop2(ctx):
+@slash.slash(name="testWhoop", guild_ids=guild_ids, description="This will WHOOP your ass!")
+async def WhoopSlash(ctx):
     await ctx.send(ctx.author.mention + " WHOOP!")
 
 
