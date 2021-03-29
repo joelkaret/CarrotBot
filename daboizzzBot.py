5c57ce831d129ae556ee8b90c0053c971ade6efb
Joel Karet <joelkaret@gmail.com>
2021-03-29 18:52:04 +0100

    Slash Commands

--- daboizzzBot.py
+++ daboizzzBot.py
@@ -2,9 +2,10 @@ import os
 import discord
 from discord.ext import commands
 from discord.utils import get
+from discord_slash import SlashCommand, SlashContext
 
 client = commands.Bot(command_prefix='&&')
-
+slash = SlashCommand(bot)
 
 @client.event
 async def on_ready():
@@ -22,6 +23,20 @@ async def ping(ctx, *message):
                 role = "this is not a role"
     if role in ctx.author.roles:
         await ctx.send(role.mention + ctx.author.mention + " says " + " ".join(message))
+
+@slash.slash(name="pingDaBoiz")
+async def ping(ctx, *message):
+    daboiz = 799036164057071636
+    role = get(ctx.guild.roles, id=daboiz)
+    for i in range(0, len(message)):
+        for j in range(0, len(message[i])):
+            if message[i][j] == "@":
+                role = "this is not a role"
+    if role in ctx.author.roles:
+        await ctx.send(role.mention + ctx.author.mention + " says " + " ".join(message))
     
+@slash.slash()
+async def testWhoop(ctx, *message):
+    await ctx.send(ctx.author.mention + " WHOOP!")
 
 client.run(os.getenv("PRIVATE_KEY"))
