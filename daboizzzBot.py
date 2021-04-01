619c3b4415dca987ace2ee7d6ba7386ac6e10b6e
joelkaret <81484847+joelkaret@users.noreply.github.com>
2021-04-01 19:13:02 +0100

    Update daboizzzBot.py
    "Removing slash command for pinging da boiz"

--- daboizzzBot.py
+++ daboizzzBot.py
@@ -34,16 +34,16 @@ async def ping(ctx, *message):
     if role in ctx.author.roles:
         await ctx.send(role.mention + ctx.author.mention + " says " + " ".join(message))
 
-@slash.slash(name="pingDaBoiz", guild_ids=guild_ids, description="This will ping da boiz")
-async def pingSlash(ctx, *message):
-    daboiz = 799036164057071636
-    role = get(ctx.guild.roles, id=daboiz)
-    for i in range(0, len(message)):
-        for j in range(0, len(message[i])):
-            if message[i][j] == "@":
-                role = "this is not a role"
-    if role in ctx.author.roles:
-        await ctx.send(role.mention + ctx.author.mention + " says " + " ".join(message))
+#@slash.slash(name="pingDaBoiz", guild_ids=guild_ids, description="This will ping da boiz")
+#async def pingSlash(ctx, *message):
+#    daboiz = 799036164057071636
+#    role = get(ctx.guild.roles, id=daboiz)
+#    for i in range(0, len(message)):
+#        for j in range(0, len(message[i])):
+#            if message[i][j] == "@":
+#                role = "this is not a role"
+#    if role in ctx.author.roles:
+#        await ctx.send(role.mention + ctx.author.mention + " says " + " ".join(message))
 
 
 @client.command(name="Whoop", description="This will WHOOP your ass!")
