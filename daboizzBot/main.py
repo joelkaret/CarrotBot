4b5adeccb3533c08df0785c2f47ac4862d1d68cb
Joel Karet <joelkaret@gmail.com>
2021-04-26 17:32:30 +0100

    removed poetry cos it was broken

--- daboizzBot/main.py
+++ daboizzBot/main.py
@@ -1,11 +1,11 @@
 # Good practice to include this. 
 import os
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 from bot import create_bot
 
 
 if __name__ == "__main__":
-    #load_dotenv() # Will load environment variables from a .env file
+    load_dotenv() # Will load environment variables from a .env file
     TOKEN = os.getenv("PRIVATE_KEY")
     client = create_bot()
     #Run Bot
