52945f394b05da8ed8d2314f85722aef0346f17c
Joel Karet <joelkaret@gmail.com>
2021-04-27 20:46:41 +0100

    Renaming Files

---
+++ CarrotBot/CarrotBot/main.py
@@ -0,0 +1,11 @@
+import os
+#from dotenv import load_dotenv
+from bot import create_bot
+
+
+if __name__ == "__main__":
+    #load_dotenv() # Will load environment variables from a .env file
+    TOKEN = os.getenv("PRIVATE_KEY")
+    client = create_bot()
+    #Run Bot
+    client.run(TOKEN)
