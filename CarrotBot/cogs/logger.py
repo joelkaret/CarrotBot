d3f43da0f9b7f0da175de6a60f9bbcf0c4d439d3
Joel Karet <joelkare@gmail.com>
2022-02-13 23:15:13 +0000

    added command to test

--- CarrotBot/cogs/logger.py
+++ CarrotBot/cogs/logger.py
@@ -16,6 +16,12 @@ class logger(commands.Cog):
             await self.log_all(msg)
         self.bot.process_commands(msg)
 
+    @commands.command(name="helpmepls")
+    async def LeaderboardAdd(self, ctx):
+       await ctx.send("help")
+
+
+
     async def log_all(self, msg):
         log_guild = self.bot.get_guild(940647396461912134)
 
@@ -39,12 +45,12 @@ class logger(commands.Cog):
         log_channel = False
         for channeli in logger_channels:
             if msg.channel.name == channeli.name:
-                log_channel = channeli
+                log_channel = discord.utils.get(log_guild.channels, name="channeli.name")
         if log_channel == False: 
             log_channel = await log_guild.create_text_channel(f"{msg.channel.name}")
         
         try:
-            await log_channel.send(f"{'-'*50}\n**{msg.author.nick}**(`{msg.author}`) in \n{msg.content}")
+            await log_channel.send(f"{'-'*50}\n**{msg.author.nick}**(`{msg.author}`)\n{msg.content}")
             if msg.attachments:
                 for i in msg.attachments:
                     await log_channel.send(f"{i}")
