463751a9b3ac0712fea3bf9e054394487662bd86
Joel Karet <joelkare@gmail.com>
2022-02-13 21:40:28 +0000

    Bug Fix 4

--- CarrotBot/cogs/logger.py
+++ CarrotBot/cogs/logger.py
@@ -16,23 +16,23 @@ class logger(commands.Cog):
         self.bot.process_commands(msg)
 
     async def log_all(self, msg):
-        loggerguild = self.bot.get_guild(940647396461912134)
+        log_guild = self.bot.get_guild(940647396461912134)
 
-        logger_categories = discord.utils.get(loggerguild.categories)
+        logger_categories = discord.utils.get(log_guild.categories)
         log_category = False
         for category in logger_categories:
             if msg.category.name == category.name:
                 log_category = category.name
-        if not log_category:
-            log_category = await loggerguild.create_category(category)
+        if log_category == False:
+            log_category = await log_guild.create_category(category)
 
-        logger_channels = discord.utils.get(loggerguild.log_category.channels)
+        logger_channels = discord.utils.get(log_guild.log_category.channels)
         log_channel = False
         for channel in logger_channels:
             if msg.channel.name == channel.name:
                 log_channel = channel.name
-        if not log_channel: 
-            log_channel = await loggerguild.create_text_channel(msg.channel.name, category = log_category)
+        if log_channel == False: 
+            log_channel = await log_guild.create_text_channel(msg.channel.name, category = log_category)
         
         try:
             await log_channel.send(f"{'-'*50}\n**{msg.author.nick}**(`{msg.author}`) in \n{msg.content}")
