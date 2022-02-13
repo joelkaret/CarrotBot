01d53fbe174469fe6a06a46848f6c418c78ae314
Joel Karet <joelkare@gmail.com>
2022-02-13 22:55:52 +0000

    Bug Fix 10

--- CarrotBot/cogs/logger.py
+++ CarrotBot/cogs/logger.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from discord.utils import get
+from sqlalchemy import null
 
 class logger(commands.Cog):
 
@@ -19,48 +20,29 @@ class logger(commands.Cog):
         log_guild = self.bot.get_guild(940647396461912134)
 
         logger_categories = discord.utils.get(log_guild.categories)
-        await msg.send(1)
         log_category = False
-        await msg.send(2)
-        for category in logger_categories:
-            await msg.send(3)
-            if msg.category.name == category.name:
-                await msg.send(4)
-                log_category = category
-                await msg.send(5)
+        for categoryi in logger_categories:
+            if msg.category.name == categoryi.name:
+                log_category = categoryi
         if log_category == False:
-            await msg.send(6)
-            log_category = await log_guild.create_category_channel(msg.guild.name)
-            await msg.send(7)
+            log_category = await log_guild.create_category(msg.guild.name)
 
         logger_channels = discord.utils.get(log_guild.log_category.channels)
-        await msg.send(8)
         log_channel = False
-        await msg.send(9)
-        for channel in logger_channels:
-            await msg.send(10)
-            if msg.channel.name == channel.name:
-                await msg.send(11)
-                log_channel = channel
-                await msg.send(12)
+        for channeli in logger_channels:
+            if msg.channel.name == channeli.name:
+                log_channel = channeli
         if log_channel == False: 
-            await msg.send(13)
             log_channel = await log_guild.create_text_channel(msg.channel.name, category = log_category)
-            await msg.send(14)
         
         try:
-            await msg.send(15)
             await log_channel.send(f"{'-'*50}\n**{msg.author.nick}**(`{msg.author}`) in \n{msg.content}")
-            await msg.send(16)
             if msg.attachments:
-                await msg.send(17)
                 for i in msg.attachments:
-                    await msg.send(18)
                     await log_channel.send(f"{i}")
-                    await msg.send(19)
             
         except Exception:
-            await msg.send("'logs' channel not found, or bot missing permissions")
+            print("'logs' channel not found, or bot missing permissions")
     
 def setup(bot):
     bot.add_cog(logger(bot))
