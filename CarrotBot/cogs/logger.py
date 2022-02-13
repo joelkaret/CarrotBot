5ded50a0319f48535a0467d4d81497079a08d143
Joel Karet <joelkare@gmail.com>
2022-02-13 23:01:19 +0000

    test without categories

--- CarrotBot/cogs/logger.py
+++ CarrotBot/cogs/logger.py
@@ -19,21 +19,29 @@ class logger(commands.Cog):
     async def log_all(self, msg):
         log_guild = self.bot.get_guild(940647396461912134)
 
-        logger_categories = discord.utils.get(log_guild.categories)
-        log_category = False
-        for categoryi in logger_categories:
-            if msg.category.name == categoryi.name:
-                log_category = categoryi
-        if log_category == False:
-            log_category = await log_guild.create_category(msg.guild.name)
+        # logger_categories = discord.utils.get(log_guild.categories)
+        # log_category = False
+        # for categoryi in logger_categories:
+        #     if msg.category.name == categoryi.name:
+        #         log_category = categoryi
+        # if log_category == False:
+        #     log_category = await log_guild.create_category(msg.guild.name)
 
-        logger_channels = discord.utils.get(log_guild.log_category.channels)
+        # logger_channels = discord.utils.get(log_guild.log_category.channels)
+        # log_channel = False
+        # for channeli in logger_channels:
+        #     if msg.channel.name == channeli.name:
+        #         log_channel = channeli
+        # if log_channel == False: 
+        #     log_channel = await log_guild.create_text_channel(msg.channel.name, category = log_category)
+
+        logger_channels = discord.utils.get(log_guild.channels)
         log_channel = False
         for channeli in logger_channels:
             if msg.channel.name == channeli.name:
                 log_channel = channeli
         if log_channel == False: 
-            log_channel = await log_guild.create_text_channel(msg.channel.name, category = log_category)
+            log_channel = await log_guild.create_text_channel(msg.channel.name)
         
         try:
             await log_channel.send(f"{'-'*50}\n**{msg.author.nick}**(`{msg.author}`) in \n{msg.content}")
