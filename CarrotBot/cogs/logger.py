f2c6042d1af4db55fb47569cce1bb820e8eab466
Joel Karet <joelkare@gmail.com>
2022-02-14 00:50:11 +0000

    fixes galore

--- CarrotBot/cogs/logger.py
+++ CarrotBot/cogs/logger.py
@@ -14,47 +14,49 @@ class logger(commands.Cog):
             return
         else:
             await self.log_all(msg)
-        await self.bot.process_commands(msg)
-
-    @commands.command(name="helpmepls")
-    async def LeaderboardAdd(self, ctx):
-       await ctx.send("help")
+        self.bot.process_commands(msg)
 
     async def log_all(self, msg):
         log_guild = self.bot.get_guild(940647396461912134)
 
-        # logger_categories = discord.utils.get(log_guild.categories)
-        # log_category = False
-        # for categoryi in logger_categories:
-        #     if msg.category.name == categoryi.name:
-        #         log_category = categoryi
-        # if log_category == False:
-        #     log_category = await log_guild.create_category(msg.guild.name)
+        logger_categories = log_guild.categories
+        log_category = False
+        for categoryi in logger_categories:
+            if msg.guild.name == categoryi.name:
+                log_category = categoryi
+        if log_category == False:
+            log_category = await log_guild.create_category(msg.guild.name)
 
-        # logger_channels = discord.utils.get(log_guild.log_category.channels)
-        # log_channel = False
-        # for channeli in logger_channels:
-        #     if msg.channel.name == channeli.name:
-        #         log_channel = channeli
-        # if log_channel == False: 
-        #     log_channel = await log_guild.create_text_channel(msg.channel.name, category = log_category)
-
-        logger_channels = discord.utils.get(log_guild.text_channels)
+        logger_channels = log_category.channels
         log_channel = False
         for channeli in logger_channels:
             if msg.channel.name == channeli.name:
-                log_channel = discord.utils.get(log_guild.channels, name=msg.channel.name)
+                log_channel = channeli
         if log_channel == False: 
-            await log_guild.create_text_channel(f"{msg.channel.name}")
-            log_channel = discord.utils.get(log_guild.channels, name=msg.channel.name)
+            log_channel = await log_guild.create_text_channel(msg.channel.name, category = log_category)
         
         try:
-            await log_channel.send(f"{'-'*50}\n**{msg.author.nick}**(`{msg.author}`)\n{msg.content}")
+            user_mentions = msg.mentions
+            role_mentions = msg.role_mentions
+            channel_mentions = msg.channel_mentions
+            content = str(msg.content)
+            for i in user_mentions:
+                pingmsg = f"<@!{i.id}>"
+                content = content.replace(pingmsg, f'***Ping-User:{i.name}***')
+            for i in role_mentions:
+                pingmsg = f"<@&{i.id}>"
+                content = content.replace(pingmsg, f'***Ping-Role:{i.name}***')
+            for i in channel_mentions:
+                pingmsg = f"<@#{i.id}>"
+                content = content.replace(pingmsg, f'***Channel:{i.name}***')    
+            content = content.replace('@','***PING: EVERYONE***')
+            await log_channel.send(f"{'-'*50}\n**{msg.author.nick}**(`{msg.author}`)\n{content}")
             if msg.attachments:
                 for i in msg.attachments:
                     await log_channel.send(f"{i}")
             
         except Exception:
+            print(msg.content, msg.author)
             print("'logs' channel not found, or bot missing permissions")
     
 def setup(bot):
