6fafd9c837555f89e2e2ce4cb700406f9006a5e0
joelkaret <81484847+joelkaret@users.noreply.github.com>
2022-02-28 00:45:53 +0000

    Fixes

--- CarrotBot/cogs/logger.py
+++ CarrotBot/cogs/logger.py
@@ -21,19 +21,35 @@ class logger(commands.Cog):
 
         logger_categories = log_guild.categories
         log_category = False
+        log_category_2 = False
         for categoryi in logger_categories:
             if msg.guild.name == categoryi.name:
                 log_category = categoryi
+            if f'{msg.guild.name} 2' == categoryi.name:
+                log_category_2 = categoryi
         if log_category == False:
             log_category = await log_guild.create_category(msg.guild.name)
 
+        if len(log_category.channels) > 49 and not log_category_2:
+            await log_guild.create_category(f'{msg.guild.name} 2')
+
         logger_channels = log_category.channels
         log_channel = False
+        log_channel_2 = False
         for channeli in logger_channels:
             if msg.channel.name == channeli.name:
                 log_channel = channeli
         if log_channel == False: 
-            log_channel = await log_guild.create_text_channel(msg.channel.name, category = log_category)
+            if log_category_2 != False:
+                logger_channels_2 = log_category_2.channels
+                for channeli_2 in logger_channels_2:
+                    if msg.channel.name == channeli_2.name:
+                        log_channel_2 = channeli_2
+                        log_channel = channeli_2
+                if log_channel_2 == False:
+                    log_channel = await log_guild.create_text_channel(msg.channel.name, category = log_category_2)
+            else:
+                log_channel = await log_guild.create_text_channel(msg.channel.name, category = log_category)
         
         try:
             user_mentions = msg.mentions
@@ -54,16 +70,20 @@ class logger(commands.Cog):
                 await log_channel.send(f"{'-'*50}\n```reply to {msg.reference.resolved.author}: {msg.reference.resolved.system_content}:```")
             else:
                 await log_channel.send(f"{'-'*50}")
-            await log_channel.send(f"**{msg.author.nick}**(`{msg.author}`)\n{content}")
+            try:
+                await log_channel.send(f"**{msg.author.nick}**(`{msg.author}`)\n{content}")
+            except:
+                await log_channel.send(f"**No Nick**(`{msg.author}`)\n{content}")
             for i in msg.attachments:
                 await log_channel.send(i)
             for i in msg.embeds:
                 await log_channel.send(embed=i)
             
         except Exception as e:
-            print(msg.content, msg.author)
+            print(msg.system_content, msg.author)
             print("'logs' channel not found, or bot missing permissions")
             print(e)
     
 def setup(bot):
     bot.add_cog(logger(bot))
+
