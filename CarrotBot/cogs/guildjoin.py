84b8d0d51f9c2d83e8d50dc09d9f76de96841cdb
joelkaret <81484847+joelkaret@users.noreply.github.com>
2022-04-11 00:31:39 +0100

    Update guildjoin.py

--- CarrotBot/cogs/guildjoin.py
+++ CarrotBot/cogs/guildjoin.py
@@ -3,17 +3,21 @@ from discord.ext import commands
 from discord.utils import get
 
 CHANNEL_ID = 918550313693245470
-
+ROLE_ID = 759257697954037790 #Public member
 
 
 class GuildJoin(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
- 
+    
     @commands.command(name="GuildJoin", 
                       descripition="Request to join the guild")
     async def GuildJoin(self, ctx, *message):
+        role = get(guild.roles, id=ROLE_ID)
+#         if not role in ctx.author.roles:
+#             await ctx.send(f'{ctx.author.mention} You must be a Public Member to do this. If you think you are, please do !verify (ign) to rectify this issue.')
+#             return
         for i in range(0, len(message)):
             for j in range(0, len(message[i])):
                 if message[i][j] == "@":
@@ -21,9 +25,14 @@ class GuildJoin(commands.Cog):
                     return
         message = " ".join(message)
         messages = message.split('/')
+        try:
+            x = messages[3]
+        except:
+            await ctx.send(f"{ctx.author.mention} You must seperate arguments with a '/'.")
+            return
         embed = discord.Embed(
             title = f'IGN: {messages[0]}',
-            colour = discord.Colour.dark_theme(),
+            colour = discord.Colour.blurple(),
             type='article')
         
         embed.set_author(name='Carrot Bot', 
@@ -35,13 +44,13 @@ class GuildJoin(commands.Cog):
                         value=messages[2],
                         inline=False)
         embed.add_field(name='Discovered guild:', 
-                        value=messages[3],
+                        value=messages[3:],
                         inline=False)
         CHANNEL = self.bot.get_channel(CHANNEL_ID)
         await CHANNEL.send(ctx.author.mention)
         msg = await CHANNEL.send(embed=embed)
-        await msg.add_reaction("⬆️")
-        await msg.add_reaction("⬇️")
+        await msg.add_reaction("<:agree:962855687103344690>")
+        await msg.add_reaction("<:disagree:962855799602970687>")
 
 def setup(bot):
     bot.add_cog(GuildJoin(bot))
