21d3afef8285bede5a97f0a10367ddf0cd369f40
joelkaret <81484847+joelkaret@users.noreply.github.com>
2022-04-10 04:56:37 +0100

    Update guildjoin.py

--- CarrotBot/cogs/guildjoin.py
+++ CarrotBot/cogs/guildjoin.py
@@ -28,16 +28,19 @@ class GuildJoin(commands.Cog):
         embed.set_author(name='Carrot Bot', 
                          icon_url='https://images-ext-1.discordapp.net/external/NpXCm8rHLPUP0jk-lrskAJPwGDF-LIZ-Kq6SQ6lO5EQ/%3Fsize%3D256/https/cdn.discordapp.com/avatars/825359434602512388/c884bc5b69225f2319af41207a62c5d9.png')
         embed.add_field(name='Aliases:', 
-                        value=messages[1])
+                        value=messages[1],
+                        inline=False)
         embed.add_field(name='Age:', 
-                        value=messages[2])
+                        value=messages[2],
+                        inline=False)
         embed.add_field(name='Discovered guild:', 
-                        value=messages[3])
+                        value=messages[3],
+                        inline=False)
         CHANNEL = self.bot.get_channel(958491773825585172)
         await CHANNEL.send(ctx.author.mention)
         msg = await CHANNEL.send(embed=embed)
-        await msg.add_reaction("⬇️")
         await msg.add_reaction("⬆️")
+        await msg.add_reaction("⬇️")
 
 def setup(bot):
     bot.add_cog(GuildJoin(bot))
